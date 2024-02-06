c23f0a042b3c76e785b7de53662cbabb10ee4279
Tom Brouwer <tom.brouwer@bookingexperts.nl>
2024-02-06 16:37:58 +0100

    20321 Fix liquid tag consistency not being validated in interpolations check
    Note that interpolation checks can be skipped on specific keys using
    the 'ignore_inconsistent_interpolations' property in the yaml config
    file
    
    Ref bookingexperts/support#20321

--- spec/interpolations_spec.rb
+++ spec/interpolations_spec.rb
@@ -5,9 +5,6 @@ require 'spec_helper'
 RSpec.describe 'Interpolations' do
   let!(:task) { I18n::Tasks::BaseTask.new }
 
-  let(:base_keys) { { 'a' => 'hello %{world}', 'b' => 'foo', 'c' => { 'd' => 'hello %{name}' }, 'e' => 'ok' } }
-  let(:test_keys) { { 'a' => 'hello', 'b' => 'foo %{bar}', 'c' => { 'd' => 'hola %{amigo}' }, 'e' => 'ok' } }
-
   around do |ex|
     TestCodebase.setup(
       'config/i18n-tasks.yml' => { base_locale: 'en', locales: %w[es] }.to_yaml,
@@ -19,13 +16,63 @@ RSpec.describe 'Interpolations' do
     TestCodebase.teardown
   end
 
-  it '#inconsistent_interpolation' do
-    wrong  = task.inconsistent_interpolations
-    leaves = wrong.leaves.to_a
+  context 'ruby string interpolations' do
+    let(:base_keys) { { 'a' => 'hello %{world}', 'b' => 'foo', 'c' => { 'd' => 'hello %{name}' }, 'e' => 'ok' } }
+    let(:test_keys) { { 'a' => 'hello', 'b' => 'foo %{bar}', 'c' => { 'd' => 'hola %{amigo}' }, 'e' => 'ok' } }
 
-    expect(leaves.size).to eq 3
-    expect(leaves[0].full_key).to eq 'es.a'
-    expect(leaves[1].full_key).to eq 'es.b'
-    expect(leaves[2].full_key).to eq 'es.c.d'
+    it 'detects inconsistent interpolations' do
+      wrong  = task.inconsistent_interpolations
+      leaves = wrong.leaves.to_a
+
+      expect(leaves.size).to eq 3
+      expect(leaves[0].full_key).to eq 'es.a'
+      expect(leaves[1].full_key).to eq 'es.b'
+      expect(leaves[2].full_key).to eq 'es.c.d'
+    end
+  end
+
+  context 'liquid tags' do
+    let(:base_keys) do
+      {
+        a: 'hello {{ world }}',
+        b: 'foo',
+        c: {
+          d: 'hello {{ name }}'
+        },
+        e: 'ok',
+        f: 'inconsistent {{ whitespace}}',
+        g: 'includes a {% comment %}',
+        h: 'wrong {% comment %}',
+        i: 'with localized value: {{ "thanks" | owner_invoice }}',
+        j: 'with wrong function: {{ "thanks" | owner_invoices }}'
+      }
+    end
+    let(:test_keys) do
+      {
+        a: 'hello',
+        b: 'foo {{ bar }}',
+        c: {
+          d: 'hola {{ amigo }}'
+        },
+        e: 'ok',
+        f: '{{whitespace }} inconsistentes',
+        g: 'incluye un {% comment %}',
+        h: '{% commentario %} equivocado',
+        i: 'con valor localizado: {{ "gracias" | owner_invoice }}',
+        j: 'con función incorrecta: {{ "thanks" | owner_invoice }}'
+      }
+    end
+
+    it 'detects inconsistent interpolations' do
+      wrong  = task.inconsistent_interpolations
+      leaves = wrong.leaves.to_a
+
+      expect(leaves.size).to eq 5
+      expect(leaves[0].full_key).to eq 'es.a'
+      expect(leaves[1].full_key).to eq 'es.b'
+      expect(leaves[2].full_key).to eq 'es.c.d'
+      expect(leaves[3].full_key).to eq 'es.h'
+      expect(leaves[4].full_key).to eq 'es.j'
+    end
   end
 end
